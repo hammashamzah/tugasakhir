5693dd4e25a98585a452421ed36e893d56d4ce4d
bimasah <bimasahbani.11ipa1@gmail.com>
2016-04-04 23:26:07 +0700

    bima

---
+++ lulusjuni/arrtrans.h
@@ -0,0 +1,14 @@
+#ifndef ARRTRANS_H
+#define ARRTRANS_H
+#include "objectvariable.h"
+
+
+class Arrtrans
+{
+public:
+    DataInputTrans data[JUMLAH_PLAYER];
+    Arrtrans();
+    ~Arrtrans();
+};
+
+#endif // ARRTRANS_H
